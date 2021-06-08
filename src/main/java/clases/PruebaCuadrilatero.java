d474beeb1f419156dcd9e1fe798b1ea54400a0ff
Keily Vallejo <84594486+keily-v@users.noreply.github.com>
2021-06-07 20:47:03 -0500

    Update PruebaCuadrilatero.java

--- src/main/java/clases/PruebaCuadrilatero.java
+++ src/main/java/clases/PruebaCuadrilatero.java
@@ -11,13 +11,16 @@ public class PruebaCuadrilatero {
     ob1.base   = 5;
     ob1.altura = 5;
    
-    
+    //si la base es igual a la altura, la figura es un cuadrado y sus medidas son diferentes es un rectángulo 
+        
     if (ob1.base == ob1.altura){
         
+        //impresión de área y perímetro del cuadrado 
         System.out.println("El perímetro del cuadrado es:" +ob1.peri_cuadrado());
         System.out.println("El área del cuadrado es:" +ob1.area_cuadrado());
         
     } else {
+        //impresión de área y perímetro de un rectángulo
     System.out.println("El perímetro del cuadrilatero es:" +ob1.perimetro());
     System.out.println("El área del cuadrilatero es:" +ob1.area());
     }
